13c52609a3d8fa153326397a4338faf21057d022
Evelinmq <20243ds170@utez.edu.mx>
2026-02-28 14:58:39 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import MainMenu from './Components/All/MainMenu'
+
+function App() {
+
+  return (
+    <>
+      <MainMenu />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/All/MainMenu.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './MainMenu.css';
+
+const VistaUsuarios = () => <div className="p-4">Región de Gestión de Usuarios</div>;
+const VistaReportes = () => <div className="p-4">Región de Reportes de Admin</div>;
+const VistaMaterias = () => <div className="p-4">Región de Materias</div>;
+const VistaCarreras = () => <div className="p-4">Región de Carreras</div>;
+
+
+function MainMenu() {
+  const [vistaActual, setVistaActual] = useState('Reportes');
+  const botones = ['Reportes', 'Usuarios', 'Materias', 'Carreras'];
+
+  const renderizarContenido = () => {
+    switch (vistaActual) {
+      case 'Usuarios': return <VistaUsuarios />;
+      case 'Materias': return <VistaMaterias />;
+      case 'Carreras': return <VistaCarreras />;
+      default: return <VistaReportes />;
+    }
+  };
+
+  return (
+    <div className="menu-container">
+      <div className="sidebar">
+        {botones.map((nombre) => (
+          <button
+            key={nombre}
+            onClick={() => setVistaActual(nombre)}
+            // Lógica de botón presionado
+            className={`btn-menu ${vistaActual === nombre ? 'btn-active' : 'btn-inactive'}`}
+          >
+            {nombre}
+          </button>
+        ))}
+      </div>
+
+      <main style={{ flex: 1 }}>
+        {renderizarContenido()}
+      </main>
+    </div>
+  );
+}
+
+export default MainMenu
