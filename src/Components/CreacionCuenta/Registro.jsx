f75b03fe5fc36beb114d2315c0e40300d2f3420f
SanDaniel777 <20243ds156@utez.edu.mx>
2026-03-16 20:25:49 -0600

    Nuevamente los inputs del registro

--- src/Components/CreacionCuenta/Registro.jsx
+++ src/Components/CreacionCuenta/Registro.jsx
@@ -57,7 +57,19 @@ function Registro() {
                     </div>
                     <div className="input-group">
                     <label >Cuatrimestre</label>
-                    <input type="text" id="cuatrimestre" placeholder="Ingrese su cuatrimestre" />
+                    <select id="cuatrimestre">
+                        <option value="">Selecciona tu cuatrimestre:</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
                     </div>
                     </div>
 
